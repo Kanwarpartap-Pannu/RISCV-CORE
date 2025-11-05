0f9a380025fccdbbc77b347c87d7eac3c74f3e52
Kanwar Partap Pannu <kanwarp@gsp15.eecs.yorku.ca>
2025-11-05 14:50:27 -0500

    initial implement of writeback, added program data mem instance and wired all modules properly

--- project/pd4/design/code/pd4.sv
+++ project/pd4/design/code/pd4.sv
@@ -21,6 +21,246 @@ module pd4 #(
   *
   */
 
+  // DECODE stage signals
+    logic [AWIDTH-1:0] d_pc;
+    logic [DWIDTH-1:0] d_insn;
+    logic [6:0]        d_opcode;
+    logic [4:0]        d_rd;
+    logic [4:0]        d_rs1;
+    logic [4:0]        d_rs2;
+    logic [6:0]        d_funct7;
+    logic [2:0]        d_funct3;
+    logic [4:0]        d_shamt;
+    logic [DWIDTH-1:0] d_imm;
+
+    // CONTROL Signals
+    logic              ctrl_pcsel;
+    logic              ctrl_immsel;
+    logic              ctrl_regwren;
+    logic              ctrl_rs1sel;
+    logic              ctrl_rs2sel;
+    logic              ctrl_memren;
+    logic              ctrl_memwren;
+    logic [1:0]        ctrl_wbsel;
+    logic [3:0]        ctrl_alusel;
+
+    // imemory signals
+    logic [DWIDTH - 1:0] addr_i;
+    logic [DWIDTH - 1:0] data_i;
+    logic write_en;
+    logic read_en;
+       
+    // Fetch signals
+    logic [DWIDTH - 1:0] f_pc;
+    logic [DWIDTH - 1:0] f_insn;
+
+    // Alu signals
+    logic [DWIDTH - 1:0] rs1_i; // not to be confused as registers
+    logic [DWIDTH - 1:0] rs2_i; // simply two operand inputs
+    logic [DWIDTH - 1:0] alu_res;
+    logic                br_taken;
+
+    //register file signals
+    logic [DWIDTH - 1:0] datawb_i;
+    logic [DWIDTH - 1:0] rs1data_o;
+    logic [DWIDTH - 1:0] rs2data_o;
+
+    //branch control signals
+    logic breq_o;
+    logic brlt_o;
+
+    // writeback signals
+    logic [DWIDTH - 1:0] writeback_data_o;
+    logic [DWIDTH - 1:0] next_pc_o;
+    logic [DWIDTH - 1:0] memory_data_i;
+
+    // Instruction Memory
+    memory #(
+        .AWIDTH(32),
+        .DWIDTH(32),
+        .BASE_ADDR(32'h01000000)
+       ) memory1 (
+        .clk(clk),
+        .rst(reset),
+        .addr_i(f_pc),
+        .data_i(data_i),
+        .read_en_i(read_en),
+        .write_en_i(write_en),
+        .data_o(f_insn)
+   );
+
+    // Default memory signals
+    assign read_en = 1'b1;
+    assign write_en = 1'b0;
+
+    // Fetch
+    fetch #(
+        .AWIDTH(32),
+        .DWIDTH(32),
+        .BASEADDR(32'h01000000)
+    ) fetch1 (
+        .clk(clk),
+        .rst(reset),
+        .pc_o(f_pc),           
+        .insn_o(f_insn)         
+    );
+
+    // Decode stage
+    decode #(
+        .AWIDTH(AWIDTH),
+        .DWIDTH(DWIDTH)
+    ) u_decode (
+        .clk(clk),
+        .rst(reset),
+        .insn_i(f_insn),
+        .pc_i(f_pc),
+        .pc_o(d_pc),
+        .insn_o(d_insn),
+        .opcode_o(d_opcode),
+        .rd_o(d_rd),
+        .rs1_o(d_rs1),
+        .rs2_o(d_rs2),
+        .funct7_o(d_funct7),
+        .funct3_o(d_funct3),
+        .shamt_o(d_shamt),
+        .imm_o(d_imm)
+    );
+
+    // Control unit
+    control #(
+        .DWIDTH(DWIDTH)
+    ) u_control (
+        .insn_i(d_insn),
+        .opcode_i(d_opcode),
+        .funct7_i(d_funct7),
+        .funct3_i(d_funct3),
+
+        .pcsel_o(ctrl_pcsel),
+        .immsel_o(ctrl_immsel),
+        .regwren_o(ctrl_regwren),
+        .rs1sel_o(ctrl_rs1sel),
+        .rs2sel_o(ctrl_rs2sel),
+        .memren_o(ctrl_memren),
+        .memwren_o(ctrl_memwren),
+        .wbsel_o(ctrl_wbsel),
+        .alusel_o(ctrl_alusel)
+    );
+
+    // Select rs1 and rs2 inputs to ALU based on control signals
+    assign rs1_i = (ctrl_rs1sel) ?  rs1data_o : d_pc;
+    assign rs2_i = (ctrl_rs2sel) ?  rs2data_o : d_imm;
+    
+    // Branch control unit
+    branch_control #(
+        .DWIDTH(DWIDTH)
+    ) u_branch_control (
+        .opcode_i(d_opcode),
+        .funct3_i(d_funct3),
+        .rs1_i(rs1data_o), // from register file becuase
+        .rs2_i(rs2data_o), // rs1_i and rs2_i will be muxed to select pc and imm rather than register data
+        .breq_o(breq_o),
+        .brlt_o(brlt_o)
+    );
+
+
+    // Excute stage - ALU
+    alu #(
+        .DWIDTH(DWIDTH),
+        .AWIDTH(AWIDTH)
+    ) u_alu (
+        .pc_i(d_pc),
+        .rs1_i(rs1_i), 
+        .rs2_i(rs2_i), 
+        .funct3_i(d_funct3),
+        .funct7_i(d_funct7),
+        .opcode_i(d_opcode),
+        .alusel_i(ctrl_alusel),
+        .eq(breq_o),
+        .lt(brlt_o),
+        .res_o(alu_res),      
+        .brtaken_o(br_taken)   
+    );
+
+
+
+    // Register File
+    register_file #(
+        .DWIDTH(DWIDTH)
+    ) u_register_file (
+        .clk(clk),
+        .rst(reset),
+        .rs1_i(d_rs1),
+        .rs2_i(d_rs2),
+        .rd_i(d_rd),
+        .datawb_i(writeback_data_o),      
+        .regwren_i(ctrl_regwren),
+        .rs1data_o(rs1data_o),     
+        .rs2data_o(rs2data_o)      
+    );   
+
+    // Ok up to here my pipeline handles fetch, decode, execute stages
+    // but fetch needs to know which pc to fetch from based on branch taken or not so it needs to be modifed to include that logic
+    // next we have our result which is correct for all stages so we need to mux and decide wether to write back alu result or memory data
+    // we need to implement memory for data memory access stage which I think is easiest if we just have another memory instance for data memory
+
+    // Program Data Memory
+    memory #(
+        .AWIDTH(32),
+        .DWIDTH(32),
+        .BASE_ADDR(32'h01000000)
+       ) memory1 (
+        .clk(clk),
+        .rst(reset),
+        .addr_i(alu_res),
+        .data_i(rs2data_o),
+        .read_en_i(read_en),
+        .write_en_i(write_en),
+        .data_o(memory_data_i) // 
+   );
+
+
+    writeback #(
+        .DWIDTH(DWIDTH),
+        .AWIDTH(AWIDTH)
+    ) u_writeback (
+        .pc_i(d_pc),
+        .alu_res_i(alu_res),
+        .memory_data_i(memory_data_i), 
+        .wbsel_i(ctrl_wbsel),
+        .brtaken_i(br_taken),
+        .writeback_data_o(writeback_data_o),
+        .next_pc_o(next_pc_o)
+    );
+
+
+
+    // Probes (required by testbench)
+
+    `define PROBE_F_PC      f_pc
+    `define PROBE_F_INSN    f_insn
+
+    `define PROBE_D_PC      d_pc
+    `define PROBE_D_OPCODE  d_opcode
+    `define PROBE_D_RD      d_rd
+    `define PROBE_D_FUNCT3  d_funct3
+    `define PROBE_D_RS1     d_rs1
+    `define PROBE_D_RS2     d_rs2
+    `define PROBE_D_FUNCT7  d_funct7
+    `define PROBE_D_IMM     d_imm
+    `define PROBE_D_SHAMT   d_shamt
+
+    `define PROBE_R_WRITE_ENABLE  ctrl_regwren  // ??
+    `define PROBE_R_WRITE_DESTINATION  d_rd // ??
+    `define PROBE_R_WRITE_DATA datawb_i // ??
+    `define PROBE_R_READ_RS1 d_rs1  // ??
+    `define PROBE_R_READ_RS2 d_rs2  // ??
+    `define PROBE_R_READ_RS1_DATA rs1data_o    // ??
+    `define PROBE_R_READ_RS2_DATA  rs2data_o   // ??
+
+    `define PROBE_E_PC       d_pc         // ??
+    `define PROBE_E_ALU_RES  alu_res       // ??
+    `define PROBE_E_BR_TAKEN  br_taken       // ??
+
 
 // program termination logic
 reg is_program = 0;
--- project/pd4/design/code/writeback.sv
+++ project/pd4/design/code/writeback.sv
@@ -33,4 +33,17 @@
      * student below...
      */
 
+
+     // Mux for write-back data selection we need to mux between alu result and memory data
+    // ok essentially we have a mux that just selects between alu res
+    // or memory data or pc+4 based on wbsel input 
+    always_comb begin
+        unique case()
+            2'd0: writeback_data_o = alu_res_i;        // ALU result
+            2'd1: writeback_data_o = memory_data_i;    // Memory data
+            2'd2: writeback_data_o = pc_i + 32'd4;         // PC + 4
+            default: writeback_data_o = 0;
+        endcase
+
+    end
 endmodule : writeback
