4c66b361929b517bcc407b0c6204f8de34138fdf
Kanwar Partap Pannu <kanwarp@gsp15.eecs.yorku.ca>
2025-11-05 14:01:28 -0500

    added implementations from previous pds

--- project/pd4/design/code/decode.sv
+++ project/pd4/design/code/decode.sv
@@ -1,7 +1,50 @@
-/*
- * Module: execute
- *
- * Description: ALU implementation for execute stage.
- *
- * -------- REPLACE THIS FILE WITH THE EXECUTE MODULE DEVELOPED IN PD3 -----------
- */
+`include "constants.svh"
+
+module decode #(
+    parameter int DWIDTH = 32,
+    parameter int AWIDTH = 32
+)(
+    input  logic clk,
+    input  logic rst,
+    input  logic [DWIDTH-1:0] insn_i,
+    input  logic [AWIDTH-1:0] pc_i,
+
+    output logic [AWIDTH-1:0] pc_o,
+    output logic [DWIDTH-1:0] insn_o,
+    output logic [6:0] opcode_o,
+    output logic [4:0] rd_o,
+    output logic [4:0] rs1_o,
+    output logic [4:0] rs2_o,
+    output logic [6:0] funct7_o,
+    output logic [2:0] funct3_o,
+    output logic [4:0] shamt_o,
+    output logic [DWIDTH-1:0] imm_o
+);
+
+  
+
+    // Field extraction (combinational logic)
+    assign opcode_o = insn_i[6:0];
+    assign rd_o     = insn_i[11:7];
+    assign funct3_o = insn_i[14:12];
+    assign rs1_o    = insn_i[19:15];
+    assign rs2_o    = insn_i[24:20];
+    assign funct7_o = insn_i[31:25];
+    assign shamt_o  = insn_i[24:20];  // alias of rs2 for shift instructions
+
+    assign pc_o   = pc_i;
+    assign insn_o = insn_i;
+    always_comb begin
+    $display("[%0t] DECODE DEBUG: INS=%h ", 
+                  $time, insn_i);
+    end
+    // Immediate Generation (through the provided igen module)
+    igen #(
+        .DWIDTH(DWIDTH)
+    ) u_igen (
+        .opcode_i(opcode_o),
+        .insn_i(insn_o),
+        .imm_o(imm_o)
+    );
+
+endmodule : decode
